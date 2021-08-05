83f9dd431deee733dc100a7e7c13d6c91332412c
Obote <obotedenis256@gmail.com>
2021-08-05 23:10:48 +0300

    Add button

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const ham = document.getElementById('hamburger'); 
 const men = document.getElementById('menu-items');
 const ul = document.getElementById('items');
+const mg = document.querySelector("mg");
 
 ham.addEventListener('click',() => {
      men.classList.toggle('bgShow');
+    //  ham.innerHTML = "<button type='button'>X</button>";
+     
 }); 
