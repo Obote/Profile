8fe2dd1f4f5bb9467008ff21494b7f41172afdc9
Obote <obotedenis256@gmail.com>
2021-08-05 23:36:35 +0300

    Add close button function

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const ham = document.getElementById('hamburger');
 const men = document.getElementById('menu-items');
 const ul = document.getElementById('items');
 const mg = document.querySelector("mg");
+const btn = document.querySelector('x');
 
 ham.addEventListener('click',() => {
      men.classList.toggle('bgShow');
-    //  ham.innerHTML = "<button type='button'>X</button>";
-     
+       
 }); 
+ 
+btn.onClick = () => {
+    men.style.display = 'none';
+    ul.style.display = 'none';
+}
