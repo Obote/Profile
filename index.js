04f0e77573211e424b67f889346d9bb3f6544fe6
Obote <obotedenis256@gmail.com>
2021-08-06 09:03:08 +0300

    Fix linters errors

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const ham = document.getElementById('hamburger'); 
+const ham = document.getElementById('hamburger');
 const men = document.getElementById('menu-items');
-const ul = document.getElementById('items');
 
+ham.addEventListener('click', () => {
+  men.classList.toggle('bgShow');
+});
 
-ham.addEventListener('click',() => {
-     men.classList.toggle('bgShow');
-            
-}); 
-
-
-men.addEventListener('click',() => {
-     men.classList.remove('bgShow');
+men.addEventListener('click', () => {
+  men.classList.remove('bgShow');
 });
