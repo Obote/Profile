62ce61f04b6cad58b92e702f12b7d09813147e8a
Obote <obotedenis256@gmail.com>
2021-08-06 15:43:57 +0300

    Add objects

---
+++ Javascript/index.js
@@ -0,0 +1,39 @@
+const ham = document.getElementById('hamburger');
+const men = document.getElementById('menu-items');
+
+ham.addEventListener('click', () => {
+  men.classList.toggle('bgShow');
+});
+
+men.addEventListener('click', () => {
+  men.classList.remove('bgShow');
+});
+
+// popup 
+
+const projects = [
+  {
+    title: 'About the project #1',
+    description: 'Descript',
+    tech: ['HTML', 'JavaScript', 'Css', 'Ruby'],
+    imgUrl: './assets/images/bg.jpg',
+  },
+  {
+    title: 'About the project #2',
+    description: 'Description of the project, date and languages',
+    tech: ['HTML', 'JavaScript', 'Css', 'Ruby'],
+    imgUrl: './assets/images/bg.jpg',
+  },
+  {
+    title: 'About the project #3',
+    description: 'Description of the project, date and languages',
+    tech: ['HTML', 'JavaScript', 'Css', 'Ruby'],
+    imgUrl: './assets/images/bg.jpg',
+  },
+  {
+    title: 'About the project #4',
+    description: 'Description of the project, date and languages',
+    tech: ['HTML', 'JavaScript', 'Css', 'Ruby'],
+    imgUrl: './assets/images/bg.jpg',
+  },
+];
