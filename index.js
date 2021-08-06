1e73170fdf478728cc5ad370cec065f3437b8f54
Obote <obotedenis256@gmail.com>
2021-08-06 08:26:49 +0300

    Add review changes required

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const ham = document.getElementById('hamburger'); 
 const men = document.getElementById('menu-items');
 const ul = document.getElementById('items');
-const mg = document.querySelector("mg");
-const btn = document.querySelector('x');
+
 
 ham.addEventListener('click',() => {
      men.classList.toggle('bgShow');
-       
+            
 }); 
- 
-btn.onClick = () => {
-    men.style.display = 'none';
-    ul.style.display = 'none';
-}
+
+
+men.addEventListener('click',() => {
+     men.classList.remove('bgShow');
+});
