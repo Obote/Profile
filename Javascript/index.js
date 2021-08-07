de8999a9d0d7d6a35c107fe04ab63c8ea8b30788
Obote <obotedenis256@gmail.com>
2021-08-07 17:30:35 +0300

    create a popup window

--- Javascript/index.js
+++ Javascript/index.js
@@ -49,64 +49,20 @@ const updateFormValues = () => {
 
 form.addEventListener('input', updateFormValues);
 
-// pop up
+const modal = document.getElementById('myModal');
+const btn = document.getElementById("project-button");
+const span = document.getElementsByClassName('close')[0];
 
-const projects = [
-  {
-    name: 'The one',
-    imgUrl: './assets/images/bg.jpg',
-    tech: ['HTML', 'Ruby on rails', 'css'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-  },
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
 
-  {
-    name: 'The two',
-    imgUrl: './assets/images/bg.jpg',
-    tech: ['HTML', 'Ruby on rails', 'css'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-  },
+span.onclick = function () {
+  modal.style.display = 'none';
+};
 
-  {
-    name: 'The three',
-    imgUrl: './assets/images/bg.jpg',
-    tech: ['HTML', 'Ruby on rails', 'css'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-  },
-
-  {
-    name: 'The four',
-    imgUrl: './assets/images/bg.jpg',
-    tech: ['HTML', 'Ruby on rails', 'css'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-  },
-];
-
-function createProjectSection() {
-  if (projects.length > 0) {
-    const workSection = document.querySelector('.workSection');
-    // let idIteration = 0;
-    for (let ii = 0; ii < projects.length; ii + 1) {
-      let techHTML = '';
-      for (let jj = 0; jj < projects[ii].tech.length; jj += 1) {
-        techHTML += `<p class="techItemsBasic techItems1">${projects[ii].tech[jj]}</p>`;
-      }
-      workSection.innerHTML += '<div class="gridContainer flexItemProjects">'
-      + `<div class="gridItem1 projectImageContainer"><img class="projectImage" src="${projects[ii].imgUrl}"></div>`
-      + '<div class="gridItem2">'
-      + '<div class="flexContainer flexColumnCenteraxis workTitle fontFamily">'
-      + '<div class="flexItem1 flexContainer flexColumnCenterAxis">'
-      + `<h3 class="cardFont cardTitle fontFamily">${projects[ii].name}</h3>`
-      + '</div>'
-      + '<div class="flexItem2 flexContainer techItems-font fontFamily">'
-      + `${techHTML}`
-      + '</div>'
-      + '<div class="flexItem1 flexContainer flexColumnCenterAxis">'
-      + '<a href="#" class="bttn fontFamily popup" id="0">See Project</a>'
-      + '</div>'
-      + '</div>'
-      + '</div>';
-      // idIteration++
-    }
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
-}
-createProjectSection();
+};
