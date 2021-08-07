a9dcb2d176bc844e80eee8c05b16090cfffd93f9
Obote <obotedenis256@gmail.com>
2021-08-07 19:54:06 +0300

    Create object function

--- Javascript/index.js
+++ Javascript/index.js
@@ -50,7 +50,7 @@ const updateFormValues = () => {
 form.addEventListener('input', updateFormValues);
 
 const modal = document.getElementById('myModal');
-const btn = document.getElementById("project-button");
+const btn = document.getElementById('project-button');
 const span = document.getElementsByClassName('close')[0];
 
 btn.onclick = function () {
@@ -66,3 +66,57 @@ window.onclick = function (event) {
     modal.style.display = 'none';
   }
 };
+
+const project = [
+  {
+    title: 'My Project',
+    image: './assets/images/Portfolio.svg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    tech: ['html', 'Ruby on rail', 'css', 'Github'],
+  },
+  {
+    title: 'My Project',
+    image: './assets/images/Portfolio.svg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    tech: ['html', 'Ruby on rail', 'css', 'Github'],
+  },
+  {
+    title: 'My Project',
+    image: './assets/images/Portfolio.svg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    tech: ['html', 'Ruby on rail', 'css', 'Github'],
+  },
+  {
+    title: 'My Project',
+    image: './assets/images/Portfolio.svg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    tech: ['html', 'Ruby on rail', 'css', 'Github'],
+  },
+];
+
+const modalFunction = (array) => {
+  const root = document.getElementById('myModal');
+  array.map((element) => {
+    const title = document.createElement('DIV');
+    title.setAttribute('class', 'title');
+    title.textContent = `${element.title}`;
+    root.appendChild(title);
+
+    const image = document.createElement('DIV');
+    image.setAttribute('class', 'decription');
+    image.textContent = `${element.image}`;
+    root.appendChild(image);
+
+    const decription = document.createElement('DIV');
+    decription.setAttribute('class', 'decription');
+    decription.textContent = `${element.description}`;
+    root.appendChild(decription);
+
+    const tech = document.createElement('DIV');
+    tech.setAttribute('class', 'tech');
+    tech.textContent = `${element.tech}`;
+    root.appendChild(tech);
+  });
+};
+
+modalFunction(project);
