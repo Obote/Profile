da15342eaea143f68385dca69fb334a039df2702
Obote <obotedenis256@gmail.com>
2021-08-07 23:13:59 +0300

    Add popup style

--- Javascript/index.js
+++ Javascript/index.js
@@ -51,24 +51,6 @@ const updateFormValues = () => {
 
 form.addEventListener('input', updateFormValues);
 
-// const modal = document.getElementById('myModal');
-// const btn = document.getElementById('project-button');
-// const span = document.getElementsByClassName('close')[0];
-
-// btn.onclick = function () {
-//   modal.style.display = 'block';
-// };
-
-// span.onclick = function () {
-//   modal.style.display = 'none';
-// };
-
-// window.onclick = function (event) {
-//   if (event.target === modal) {
-//     modal.style.display = 'none';
-//   }
-// };
-
 const projects = [
   {
     title: 'My Project',
