5077515c9be85d5b9a411cb0cb712cad88a9d2c6
Obote <obotedenis256@gmail.com>
2021-08-07 21:03:34 +0300

    Add style

--- Javascript/index.js
+++ Javascript/index.js
@@ -19,7 +19,9 @@ form1.addEventListener('submit', (event) => {
   if (validMail.test(emailInput.value)) {
     document.getElementById('lowerCase1').classList.toggle('formShow');
     event.preventDefault();
-  } else if (!document.getElementById('lowerCase1').className === 'mailErorr formShow') {
+  } else if (
+    !document.getElementById('lowerCase1').className === 'mailErorr formShow'
+  ) {
     document.getElementById('lowerCase1').classList.toggle('formShow');
   }
 });
@@ -49,74 +51,128 @@ const updateFormValues = () => {
 
 form.addEventListener('input', updateFormValues);
 
-const modal = document.getElementById('myModal');
-const btn = document.getElementById('project-button');
-const span = document.getElementsByClassName('close')[0];
+// const modal = document.getElementById('myModal');
+// const btn = document.getElementById('project-button');
+// const span = document.getElementsByClassName('close')[0];
 
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
+// btn.onclick = function () {
+//   modal.style.display = 'block';
+// };
 
-span.onclick = function () {
-  modal.style.display = 'none';
-};
+// span.onclick = function () {
+//   modal.style.display = 'none';
+// };
 
-window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-};
+// window.onclick = function (event) {
+//   if (event.target === modal) {
+//     modal.style.display = 'none';
+//   }
+// };
 
-const project = [
+const projects = [
   {
     title: 'My Project',
     image: './assets/images/Portfolio.svg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     tech: ['html', 'Ruby on rail', 'css', 'Github'],
   },
   {
     title: 'My Project',
     image: './assets/images/Portfolio.svg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     tech: ['html', 'Ruby on rail', 'css', 'Github'],
   },
   {
     title: 'My Project',
     image: './assets/images/Portfolio.svg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     tech: ['html', 'Ruby on rail', 'css', 'Github'],
   },
   {
     title: 'My Project',
     image: './assets/images/Portfolio.svg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     tech: ['html', 'Ruby on rail', 'css', 'Github'],
   },
 ];
 
-const modalFunction = (array) => {
-  const root = document.getElementById('myModal');
-  array.map((element) => {
-    const title = document.createElement('DIV');
-    title.setAttribute('class', 'title');
-    title.textContent = `${element.title}`;
-    root.appendChild(title);
+const btns = document.querySelectorAll('.buttons');
+const divProject = document.createElement('div');
 
-    const image = document.createElement('DIV');
-    image.setAttribute('class', 'decription');
-    image.textContent = `${element.image}`;
-    root.appendChild(image);
+function addEventListenerList() {
+  for (let i = 0; i < btns.length; i += 1) {
+    btns[i].addEventListener('click', (event) => {
+      const projectId = event.target.id;
+      divProject.id = 'modal';
+      divProject.className = 'modal flex';
+      document.querySelector('.overal').style.display = 'flex';
 
-    const decription = document.createElement('DIV');
-    decription.setAttribute('class', 'decription');
-    decription.textContent = `${element.description}`;
-    root.appendChild(decription);
+      let techHTML = '';
+      for (let l = 0; l < projects[i].tech.length; l += 1) {
+        techHTML += `<li class="modal-language">${projects[i].tech[l]}</li>`;
+      }
 
-    const tech = document.createElement('DIV');
-    tech.setAttribute('class', 'tech');
-    tech.textContent = `${element.tech}`;
-    root.appendChild(tech);
-  });
-};
+      divProject.innerHTML = '<section class="modal-header flex">'
+        + '<div class="top-side-modal flex">'
+        + `<p>${projects[projectId].title}</p>`
+        + '<a class="close-popup">&times;</a>'
+        + '</div>'
+        + '</section>'
+        + '<div class="popup-img">'
+        + `<img width="330px" src="${projects[projectId].image}" alt="placeholder>`
+        + '</div>'
+        + '<section class="modal-section">'
+        + '<div class="modal-description">'
+        + `<p id = "P-description"class="modal-P">${projects[projectId].description}</p>`
+        + '</div>'
+        + '<div class="modal-div-language flex">'
+        + `<ul id="project-list">${techHTML}</ul>`
+        + '</div>'
+        + '<div class="modal-div-sources flex">'
+        + '<button class="modal-buttons">See Live<a href="#"></a></button>'
+        + '<button class="modal-buttons">See Source<a href="#"></a></button>'
+        + '</div>'
+        + '</section>';
 
-modalFunction(project);
+      document.getElementById('overal').appendChild(divProject);
+
+      const erase = document.querySelector('.close-popup');
+      erase.addEventListener('click', () => {
+        document.querySelector('.overal').style.display = 'none';
+        divProject.remove();
+      });
+    });
+  }
+}
+addEventListenerList();
+
+// const modalFunction = (array) => {
+//   const root = document.getElementById('myModal');
+//   array.map((element) => {
+//     const title = document.createElement('DIV');
+//     title.setAttribute('class', 'title');
+//     title.textContent = `${element.title}`;
+//     root.appendChild(title);
+
+//     const image = document.createElement('DIV');
+//     image.setAttribute('class', 'image');
+//     image.textContent = `${element.image}`;
+//     root.appendChild(image);
+
+//     const decription = document.createElement('DIV');
+//     decription.setAttribute('class', 'decription');
+//     decription.textContent = `${element.description}`;
+//     root.appendChild(decription);
+
+//     const tech = document.createElement('DIV');
+//     tech.setAttribute('class', 'tech');
+//     tech.textContent = `${element.tech}`;
+//     root.appendChild(tech);
+//   });
+// };
+
+// modalFunction(project);
